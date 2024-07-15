877c0e9c0ef44210053923f542529d80ce27d04c
Nicolas Schoonbroodt <Nicolas@mathworks.com>
2024-07-15 21:56:47 +0200

    explore env

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 import env from "../env.js";
 
 test("has title", async ({ page }) => {
-  console.log(env.INPUT_CLASS);
+  console.log(process.env);
 });
