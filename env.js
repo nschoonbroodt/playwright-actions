f4e75d8f595a059c74d4e10186f7373baca04e35
Nicolas Schoonbroodt <Nicolas@mathworks.com>
2024-07-15 22:17:53 +0200

    adding all needed env?

--- env.js
+++ env.js
@@ -2,7 +2,16 @@ import { z } from "zod";
 import "dotenv/config";
 
 const envSchema = z.object({
+  INPUT_USER: z.string(),
   INPUT_CLASS: z.string(),
+  INPUT_DAY: z.string(), // TODO coerce
+  INPUT_HOUR: z.string(), // TODO Coerce
+  INPUT_MINUTE: z.string(), // TODO Coerce
+  INPUT_LOCATION: z.string(), // TODO check
+  USER_NADA_LOGIN: z.string().email(),
+  USER_NICOLAS_LOGIN: z.string().email(),
+  USER_NADA_PASSWORD: z.string(),
+  USER_NICOLAS_PASSWORD: z.string(),
 });
 
 const env = envSchema.parse(process.env);
